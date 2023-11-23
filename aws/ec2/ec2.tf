9f04f3e645cacd242ad546a3f44d8dbd70c2692e
Luis Redda <luis.redda@gmail.com>
2023-11-23 16:04:45 -0300

    Update ec2.tf

--- aws/ec2/ec2.tf
+++ aws/ec2/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "ec2" {
   instance_type           = "t3.micro"
   user_data = file("userdata.tpl")
   vpc_security_group_ids = [ aws_security_group.allow_http.id ]
+  key_name               = var.key_name
   tags = {
     Name = "${var.name}-instance"
   }
