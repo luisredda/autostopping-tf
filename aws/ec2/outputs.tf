5ec9ecba08a0482874697fab9cfc9ee8b7f83c90
Luis Redda <luis.redda@gmail.com>
2023-11-23 17:15:05 -0300

    Create outputs.tf

---
+++ aws/ec2/outputs.tf
@@ -0,0 +1,9 @@
+output "public_ip" {
+  description = "List of public IP addresses assigned to the instances, if applicable"
+  value       = aws_instance.this.*.public_ip
+}
+
+output "id" {
+  description = "List of IDs of instances"
+  value       = aws_instance.this.*.id
+}
