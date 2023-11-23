fa8c2c96e7d57bd11b02e1298bd120280aa8d24f
Luis Redda <luis.redda@gmail.com>
2023-11-23 17:18:32 -0300

    Update outputs.tf

--- aws/ec2/outputs.tf
+++ aws/ec2/outputs.tf
@@ -1,9 +1,9 @@
 output "public_ip" {
   description = "List of public IP addresses assigned to the instances, if applicable"
-  value       = aws_instance.this.*.public_ip
+  value       = aws_instance.ec2.*.public_ip
 }
 
 output "id" {
   description = "List of IDs of instances"
-  value       = aws_instance.this.*.id
+  value       = aws_instance.ec2.*.id
 }
