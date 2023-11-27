6705757ab162184a92a63d18a53f261c9ac3fcd3
Luis Redda <luis.redda@gmail.com>
2023-11-27 11:44:41 -0300

    Update alb.tf

--- aws/ec2/alb.tf
+++ aws/ec2/alb.tf
@@ -17,7 +17,7 @@ resource "aws_security_group" "http" {
   ingress {
     description      = "TLS from VPC"
     from_port        = 80
-    to_port          = 80
+    to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
