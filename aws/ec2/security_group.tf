07cf81121ccec01dbd62c710f21a4167e4729b8b
Luis Redda <luis.redda@gmail.com>
2023-11-27 11:45:04 -0300

    Update security_group.tf

--- aws/ec2/security_group.tf
+++ aws/ec2/security_group.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "allow_http" {
   ingress {
     description      = "Open HTTP"
     from_port        = 80
-    to_port          = 80
+    to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = [ "0.0.0.0/0" ]
   }
